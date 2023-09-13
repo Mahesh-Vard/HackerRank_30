ed4d1be5d790fd4f9d63c4fd038c30909f1fa322
Akena Mahesh Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-09-13 18:34:18 +0530

    Binary Numbers

---
+++ Day_10.py
@@ -0,0 +1,13 @@
+n = int(input().strip())
+binary = str(bin(n))
+#print binary
+tot=0
+tmp=0
+for i in binary:
+    if i=='1':
+        tmp+=1
+    else:
+        tot=max(tot,tmp)
+        tmp=0
+tot=max(tot,tmp)
+print(tot)

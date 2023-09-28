200ee1335963535695b380021a1f9b2095b0b8cb
Akena Mahesh Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-09-28 07:19:09 +0530

    Abstract classs

---
+++ day_13.py
@@ -0,0 +1,2 @@
+# as Iam not perfect with day 13 I refer you to the below link, i'll update the answer with in a couple of days 
+https://programs.programmingoneonone.com/2021/01/hackerrank-day-13-abstract-classes-solution.html

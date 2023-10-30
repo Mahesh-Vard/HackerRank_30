33df10e8784719d7c9d77f3e8425e401e2702def
Akena Mahesh Vardhan <113368513+Mahesh-Vardhan@users.noreply.github.com>
2023-10-30 22:42:10 +0530

    Day_20.py

---
+++ Day_20.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = [int(x) for x in input().split(" ")]
+swaps = 0
+for i in range(n):
+    for j in range(n - 1):
+        if arr[j] > arr[j + 1]:
+            tmp = arr[j]
+            arr[j] = arr[j + 1]
+            arr[j + 1] = tmp
+            swaps += 1
+    if swaps == 0:
+        break
+print("Array is sorted in " + str(swaps) + " swaps.")
+print("First Element: " + str(arr[0]))
+print("Last Element: " + str(arr[len(arr) - 1]))
